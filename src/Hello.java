f1808818d5a125e4ec888bc606e0b4a28b48f409
Cihat Eminsoy <cihateminsoy@gmail.com>
2021-02-02 18:31:12 +0800

    project is already in remote repo.

--- src/Hello.java
+++ src/Hello.java
@@ -2,4 +2,10 @@ public class Hello {
 
     //more stuff
     //Mac and window users are happy to see commit dialog finally
+
+    //I have already published on GitHub.com
+    //now I am ready to make more commits
+    //but do I need to share again?
+    //Absolutely no!
+    //It's time to commit and just push!!!!
 }
